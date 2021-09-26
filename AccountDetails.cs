b4abe685a14d852351e74000dbe5ab6a88d4ad19
Sathishkumar11055 <74650240+Sathishkumar11055@users.noreply.github.com>
2021-09-26 18:33:53 +0530

    Add files via upload

---
+++ AccountDetails.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class AccountDetails
+    {
+        protected int accNo;
+        protected string name;
+        protected string accType;
+        protected double balance;
+
+        public void getData()
+        {
+            Console.WriteLine("Enter Account Number");
+            accNo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Account Type");
+            accType = Console.ReadLine();
+            Console.WriteLine("Enter Balance");
+            balance = double.Parse(Console.ReadLine());
+        }
+
+        public Double checkBalance()
+        {
+            return this.balance;
+        }
+
+        public void deposit()
+        {
+            Console.WriteLine("Enter Amount");
+            double amount = double.Parse(Console.ReadLine());
+            this.balance = this.balance + amount;
+            
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine("Enter Amount");
+            double amount = double.Parse(Console.ReadLine());
+            if (amount < this.balance)
+            {
+                this.balance = this.balance - amount;
+            }
+            else
+                Console.WriteLine("Withdraw was greater then balance");
+        }
+
+        public Boolean checkAccount(int accNo)
+        {
+            if (this.accNo == accNo)
+                return true;
+            else
+                return false;
+        }
+
+        public void displayAccountDetail()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bank Account Number  = "+accNo);
+            Console.WriteLine("Account Holder Name  = " + name);
+            Console.WriteLine("Bank Account Type    = " + accType);
+            Console.WriteLine("Balance of Account   = " + balance);
+        }
+    }
+}
---
+++ BankApplication.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class BankApplication
+    {
+        static void Main(string[] args)
+        {
+            AccountDetails ob = new AccountDetails();
+            int ch, accNo;
+            do
+            {
+                Console.WriteLine("1. Create Account\n2. Display Account Details\n3. Current Balance" +
+                    "\n4. Deposit\n5. Withdraw\n6 Exits\n");
+                Console.WriteLine("Enter Your Choice");
+                ch = int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        ob.getData();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Account Number");
+                        accNo = int.Parse(Console.ReadLine());
+                        if (ob.checkAccount(accNo))
+                        {
+                            ob.displayAccountDetail();
+                        }
+                        else
+                            Console.WriteLine("Don't have a Account");
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Account Number");
+                        accNo = int.Parse(Console.ReadLine());
+                        if (ob.checkAccount(accNo))
+                        {
+                            Console.WriteLine("Avaliable Balance is = " + ob.checkBalance());
+                        }
+                        else
+                            Console.WriteLine("Don't have a Account");
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Account Number");
+                        accNo = int.Parse(Console.ReadLine());
+                        if (ob.checkAccount(accNo))
+                        {
+                            ob.deposit();
+                        }
+                        else
+                            Console.WriteLine("Don't have a Account");
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter Account Number");
+                        accNo = int.Parse(Console.ReadLine());
+                        if (ob.checkAccount(accNo))
+                        {
+                            ob.Withdraw();
+                        }
+                        else
+                            Console.WriteLine("Don't have a Account");
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.WriteLine("Please Put valide option");
+                        break;
+                }
+            } while (ch != 6);
+        }
+    }
+}
